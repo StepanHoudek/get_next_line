20b829e9d78241813d18cac44245c4b74cb60eb6
Stepan Houdek <stepan.houdek@dcstelekom.eu>
2024-01-25 09:58:28 +0100

    Update: next_line

--- get_next_line.c
+++ get_next_line.c
@@ -16,3 +16,25 @@ char *get_next_line(int fd)
 {
 	
 }
+
+
+#include <stdio.h>
+#include <fcntl.h>
+
+int main(void) {
+    int file_descriptor;
+
+    // Open a file
+    file_descriptor = open("example.txt", O_RDONLY);
+
+    if (file_descriptor == -1) {
+        perror("Error opening file");
+    } else {
+        printf("File descriptor for example.txt: %d\n", file_descriptor);
+
+        // Close the file
+        close(file_descriptor);
+    }
+
+    return 0;
+}
