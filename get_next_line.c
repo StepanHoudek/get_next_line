4434d65a1ea0c81bdda120a197960b65e4b9950a
shoudek <stepan.houdek1@gmail.com>
2024-01-25 12:09:26 +0100

    Update: first working version. Read byte by byte until next line

--- get_next_line.c
+++ get_next_line.c
@@ -6,35 +6,65 @@
 /*   By: shoudek <shoudek@student.42prague.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/24 16:03:36 by shoudek           #+#    #+#             */
-/*   Updated: 2024/01/24 16:03:36 by shoudek          ###   ########.fr       */
+/*   Updated: 2024/01/25 12:09:03 by shoudek          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+#include <stdio.h>
 
-char *get_next_line(int fd)
+static int	g_buff_size = 1024;
+static char	*buffer;
+
+char	*get_next_line(int fd)
 {
-	
+	char	*buff;
+	char	*ptr;
+	int		j;
+	int		bytes_read;
+	int		i;
+
+	buff = malloc(sizeof(char) * g_buff_size);
+	if (!buff)
+		return (NULL);
+	bytes_read = 1;
+	i = 0;
+	while (bytes_read && *(buff++) != '\n')
+	{
+		bytes_read = read(fd, buff, 1);
+		if (bytes_read != 1)
+			return (NULL);
+		i++;
+	}
+	ptr = malloc(sizeof(char) * i);
+	if (!ptr)
+		return (NULL);
+	j = 0;
+	while (i > j)
+	{
+		ptr[j] = buff[j - i];
+		j++;
+	}
+	i++;
+	return (ptr);
 }
 
-
-#include <stdio.h>
 #include <fcntl.h>
+#include <stdio.h>
 
-int main(void) {
-    int file_descriptor;
+int	main(void)
+{
+	char	*path;
+	int		fd;
+	char	*ptr;
 
-    // Open a file
-    file_descriptor = open("example.txt", O_RDONLY);
-
-    if (file_descriptor == -1) {
-        perror("Error opening file");
-    } else {
-        printf("File descriptor for example.txt: %d\n", file_descriptor);
-
-        // Close the file
-        close(file_descriptor);
-    }
-
-    return 0;
+	path = "example.txt";
+	fd = open(path, O_RDONLY);
+	ptr = get_next_line(fd);
+	printf("%s", ptr);
+	while (ptr)
+	{
+		ptr = get_next_line(fd);
+		printf("%s", ptr);
+	}
 }
