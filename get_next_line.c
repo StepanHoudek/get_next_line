50587cc0715d5e6fa7edfaa9fbfdbe83673ed4dc
shoudek <stepan.houdek1@gmail.com>
2024-01-25 15:02:45 +0100

    Update: implementing pseudo code

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: shoudek <shoudek@student.42prague.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/24 16:03:36 by shoudek           #+#    #+#             */
-/*   Updated: 2024/01/25 14:32:35 by shoudek          ###   ########.fr       */
+/*   Updated: 2024/01/25 14:59:48 by shoudek          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,22 +28,94 @@ char	*ft_strchr(const char *s, int c)
 	return (0);
 }
 
-char	*get_next_line(int fd)
+size_t	ft_strlen(const char *s)
 {
-	// If EOF in buffer_ptr
+	size_t	size;
+
+	size = 0;
+	while (*s)
+	{
+		size++;
+		s++;
+	}
+	return (size);
+}
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char			*substr;
+	unsigned int	i;
+
+	if ((size_t)start + len > ft_strlen(s))
+		len = ft_strlen(s) - (size_t)start;
+	if ((size_t)start >= ft_strlen(s))
+	{
+		substr = malloc(sizeof(char));
+		if (!substr)
+			return (0);
+		substr[0] = 0;
+		return (substr);
+	}
+	substr = malloc(sizeof(char) * len + 1);
+	if (!substr)
+		return (0);
+	i = 0;
+	while (s[i + start] != '\0' && i < (unsigned int)len)
+	{
+		substr[i] = s[i + start];
+		i++;
+	}
+	substr[i] = '\0';
+	return (substr);
+}
+
+
+	// If no characters in buffer
+	// 		read the buffer_size from the file and store in buffer
+	// If EOF in buffer
 	// {
-	// create a substring from the buffer_ptr from start to EOF
+	// create a substring from the buffer from start to EOF
 	// return substring
 	// }
-	// Else if \n in buffer_ptr
+	// Else if \n in buffer
 	// {
-	// create a substring from the buffer_ptr from start to \n
-	// Move buffer_ptr forward just after the \n
+	// create a substring from the buffer from start to \n
+	// Move buffer forward just after the \n
 	// return substring
 	// }
 	// else
-	// read the buffer_size from the file and store in buffer
+	//
 	// recursively call get_next_line
+
+char	*get_next_line(int fd)
+{
+	static char	*buffer;
+	char		*buffer_backup;
+	size_t		buffer_size;
+	ssize_t		bytes_read;
+
+	buffer_size = 200;
+	if (!buffer)
+	{
+		buffer = malloc(sizeof(char) * buffer_size);
+		if (!buffer)
+			return (0);
+	}
+	if (*buffer == '\0')
+		bytes_read = read(fd, buffer, buffer_size);
+	if (bytes_read != buffer_size)
+	{
+		return (ft_substr(buffer, 0, bytes_read));
+	}
+	if (ft_strchr(buffer, '\n'))
+	{
+		buffer_backup = buffer;
+		buffer = ft_strchr(buffer, '\n');
+		return (ft_substr(buffer_backup, 0, ft_strchr(buffer_backup, '\n')
+				- buffer_backup));
+	}
+	else
+		get_next_line(fd);
 }
 
 int	main(void)
