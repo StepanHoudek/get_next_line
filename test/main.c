d4de4c7f1b20ab9d3d78737308172a61b708b8c0
shoudek <stepan.houdek1@gmail.com>
2024-01-25 17:04:52 +0100

    Update: adding tests

--- test/main.c
+++ test/main.c
@@ -6,28 +6,44 @@
 /*   By: shoudek <shoudek@student.42prague.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/24 16:13:41 by shoudek           #+#    #+#             */
-/*   Updated: 2024/01/24 16:13:41 by shoudek          ###   ########.fr       */
+/*   Updated: 2024/01/25 17:02:21 by shoudek          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../get_next_line.h"
+
+// cc -Wall -Wextra -Werror -D BUFFER_SIZE=1 *.c ./test/main.c && ./a.out
+
 #include <stdio.h>
-#include <fcntl.h>
 
-int main(void) {
-    int file_descriptor;
-
-    // Open a file
-    file_descriptor = open("example.txt", O_RDONLY);
-
-    if (file_descriptor == -1) {
-        perror("Error opening file");
-    } else {
-        printf("File descriptor for example.txt: %d\n", file_descriptor);
-
-        // Close the file
-        close(file_descriptor);
-    }
-
-    return 0;
+void	print_string_fd(int fd, char *line)
+{
+	line = get_next_line(fd);
+	printf("Line: |%s|\n", line);
+	free(line);
 }
 
+int	main(void)
+{
+	int		fd;
+	char	*line;
+
+	line = NULL;
+	fd = open("example.txt", O_RDONLY);
+	if (fd == -1)
+		write(2, "Problem opening file.", 21);
+	print_string_fd(fd, line);
+	print_string_fd(fd, line);
+	print_string_fd(fd, line);
+	close(fd);
+	fd = open("example.txt", O_RDONLY);
+	if (fd == -1)
+		write(2, "Problem opening file.", 21);
+	print_string_fd(fd, line);
+	print_string_fd(fd, line);
+	print_string_fd(fd, line);
+	print_string_fd(fd, line);
+	print_string_fd(fd, line);
+	close(fd);
+	return (0);
+}
